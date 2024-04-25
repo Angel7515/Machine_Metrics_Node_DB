bc875b2309f1b5c8f53527f1e11eaef341f56c0c
Angel7515 <77angelayala55@gmail.com>
2024-04-25 16:01:06 -0600

    percentage 1.0

--- Machine_Metrics_KMIT_Node/models/OverviewHome.js
+++ Machine_Metrics_KMIT_Node/models/OverviewHome.js
@@ -24,26 +24,16 @@ class ProjectSummary {
           GROUP BY k2.idkpis)
       GROUP BY k.project_idproject;`; */
 
-      const query = `
+    const query = `
       SELECT 
         k.project_idproject,
-        SUM(p.kpi_str_porcent) AS total_porcentaje,
+        SUM(COALESCE(p.kpi_str_porcent, 0)) AS total_porcentaje,
         COUNT(*) AS numero_elementos,
         MAX(p.date_upload) AS fecha_ultima_actualizacion
       FROM
         kmit_projects_dashboard.kpis k
       LEFT JOIN
         kmit_projects_dashboard.performance p ON k.idkpis = p.kpis_idkpis
-      WHERE
-        p.idperformance IN (SELECT 
-            MAX(idperformance)
-          FROM
-            kmit_projects_dashboard.performance p2
-          JOIN
-            kmit_projects_dashboard.kpis k2 ON p2.kpis_idkpis = k2.idkpis
-          WHERE
-            k2.project_idproject = k.project_idproject
-          GROUP BY k2.idkpis)
       GROUP BY k.project_idproject;`;
 
     db.query(query, callback);
