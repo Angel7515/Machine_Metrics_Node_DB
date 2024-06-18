feac946960c0fa55877bb139204c61afaf82d01f
Angel7515 <77angelayala55@gmail.com>
2024-06-18 09:51:41 -0600

    Observaciones a Desarrollo

--- Data_Base/DataBaseI.sql
+++ Data_Base/DataBaseI.sql
@@ -2,5 +2,4 @@ USE kmit_projects_dashboard;
 
 INSERT INTO person (idactive, full_name, user_role) 
 VALUES ('db01ae01-6f86-44c7-8cf7-be8de9bdc29f', 'ANDRADE AYALA, Miguel Angel (CIMMYT)', 'ADMIN'),
-('013153aa-f8ee-4386-b7b5-135022bffaca', 'vero', 'LEADER'),
-('8d4765f0-80c9-4f71-8647-a81658ff90db', 'ale', 'LEADER');
+('8d4765f0-80c9-4f71-8647-a81658ff90db', 'TENORIO ROBLES, Alejandra (CIMMYT)', 'LEADER');
